73868e4b56ab2ff2fc01043914dad19e16cd7f61
Felipe Romao <feliperomao.a@gmail.com>
2021-02-06 13:00:33 -0300

    Add meta tag for security policy

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,10 @@ import GlobalStyle from "../styles/GlobalStyle";
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <meta
+        http-equiv="Content-Security-Policy"
+        content="upgrade-insecure-requests"
+      />
       <GlobalStyle />
       <Component {...pageProps} />
     </>
