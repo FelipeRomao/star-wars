7c6f5c3e98c48c6189909c53fc8a3d45a9d1e482
Felipe Romao <feliperomao.a@gmail.com>
2021-02-06 13:07:31 -0300

    Changes for meta tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,10 +3,6 @@ import GlobalStyle from "../styles/GlobalStyle";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <meta
-        http-equiv="Content-Security-Policy"
-        content="upgrade-insecure-requests"
-      />
       <GlobalStyle />
       <Component {...pageProps} />
     </>
